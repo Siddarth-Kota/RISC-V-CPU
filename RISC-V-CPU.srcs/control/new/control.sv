795df4950be0cefb8ff5144675f7316ed4b3d46e
_Kota_ <94937056+Siddarth-Kota@users.noreply.github.com>
2026-01-14 03:55:39 -0800

    started on ALU control

---
+++ RISC-V-CPU.srcs/control/new/control.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/14/2026 03:53:34 AM
+// Design Name: 
+// Module Name: control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control(
+
+    );
+endmodule
---
+++ RISC-V-CPU.srcs/control/new/control_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/14/2026 03:52:42 AM
+// Design Name: 
+// Module Name: control_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_tb(
+
+    );
+endmodule
