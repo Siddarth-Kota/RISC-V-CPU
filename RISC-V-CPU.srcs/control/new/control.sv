6e31c9abc93b861034c680ec0764305895e5ef0f
_Kota_ <94937056+Siddarth-Kota@users.noreply.github.com>
2026-01-25 23:33:38 -0800

    Created Basic Control supporting LW

--- RISC-V-CPU.srcs/control/new/control.sv
+++ RISC-V-CPU.srcs/control/new/control.sv
@@ -1,26 +1,41 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/14/2026 03:53:34 AM
-// Design Name: 
-// Module Name: control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module control(
+    input logic [6:0] op,
+    input logic [2:0] func3,
+    input logic [6:0] func7,
+    input logic alu_zero,
 
+    output logic [2:0] alu_control,
+    output logic [1:0] imm_source,
+    output logic mem_write,
+    output logic reg_write
     );
+
+    //Main Decoder
+    logic [1:0] alu_op;
+    always_comb begin
+        case (op)
+            7'b0000011: begin //lw
+                reg_write = 1'b1;
+                mem_write = 1'b0;
+                imm_source = 2'b0;
+                alu_op = 2'b00;
+            end
+            default: begin
+                reg_write = 1'b0;
+                mem_write = 1'b0;
+                imm_source = 2'b00;
+                alu_op = 2'b00;
+            end
+        endcase
+    end
+
+    //ALU Decoder
+    always_comb begin
+        case (alu_op)
+            2'b00: alu_control = 3'b000; //LW,SW: ADD
+            default: alu_control = 3'b000;
+        endcase
+    end
 endmodule
--- RISC-V-CPU.srcs/control/new/control_tb.sv
+++ RISC-V-CPU.srcs/control/new/control_tb.sv
@@ -1,26 +1,67 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/14/2026 03:52:42 AM
-// Design Name: 
-// Module Name: control_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module control_tb();
+        //inputs
+        logic [6:0] op;
+        logic [2:0] func3;
+        logic [6:0] func7;
+        logic alu_zero;
 
-module control_tb(
+        //outputs
+        logic [2:0] alu_control;
+        logic [1:0] imm_source;
+        logic mem_write;
+        logic reg_write;
 
-    );
+        //Debug
+        logic [2:0] test_num = 0;
+
+        control dut (
+            .op(op),
+            .func3(func3),
+            .func7(func7),
+            .alu_zero(alu_zero),
+            .alu_control(alu_control),
+            .imm_source(imm_source),
+            .mem_write(mem_write),
+            .reg_write(reg_write)
+        );
+
+        task set_default_vals();
+            begin
+                op = 7'bxxxxxxxx;
+                func3 = 3'bxxx;
+                func7 = 7'bxxxxxxxx;
+                alu_zero = 1'bx;
+                #1;
+                $display("Set Inputs to X");
+            end
+        endtask
+
+        initial begin
+            $display("Starting Control Unit Testbench");
+            test_num = 1;
+            set_default_vals();
+            #1;
+            op = 7'b0000011; // lw
+            #1;
+            
+            $display("Test LW Instruction:");
+
+            func3 = 3'b000;
+            func7 = 7'b0000000;
+            alu_zero = 1'b0;
+            #1;
+            assert (alu_control === 3'b000) else $error("Assertion failed: alu_control != 000 (Got %b)", alu_control);
+            assert (imm_source  === 2'b00)  else $error("Assertion failed: imm_source != 00 (Got %b)", imm_source);
+            assert (mem_write   === 1'b0)   else $error("Assertion failed: mem_write != 1 (Got %b)", mem_write);
+            assert (reg_write   === 1'b1)   else $error("Assertion failed: reg_write != 1 (Got %b)", reg_write);
+            $display("LW Instruction Test done");
+            
+            
+
+            #10;
+            $display("All tests completed");
+            $finish;
+        end
 endmodule
